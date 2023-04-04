3acc85422135bb466960e74442e957de8b641b1a
jungjieun <je_eun2735@naver.com>
2023-04-04 13:05:32 +0900

    mysql test file2

---
+++ mysql2.js
@@ -0,0 +1,76 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+const user = require('./user');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host:'127.0.0.1',
+  user:'root',
+  email:'Xxia1215@@',
+  database:'MYTEST'
+});
+connection.connect()
+// 사용자 이름 , 암호 저장
+const users = {
+  'username': 'email',
+  'jieun':'hannah_a@naver.com'
+};
+connection.query('SELECT * FROM test01', function (error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+ 
+connection.end();
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  // 이렇게 하면 들어오는 요청 개체에서 method 및 속성이 추출됨
+  if (method === 'GET') {
+    if (url === '/') {
+      fs.readFile(path.join(__dirname, 'login.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    }
+    //로그인 폼에서 전송버튼을 누르면 post 방식으로 요청이 전송
+  } else if (method === 'POST') {
+    if (url === '/login') {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        const {username, email } = qs.parse(body);
+        // const sql = `INSERT INTO test01 (name, email) VALUES ('${name}', '${email}')`;
+        // connection.query(sql, (err, result) => {
+        // if (err) throw err;
+        // console.log('User saved successfully!');
+        });
+        if (users[username] && users[email] === 'email') {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Login!');
+        } else {
+          res.writeHead(401, { 'Content-Type': 'text/plain' });
+          res.end("doesn't match");
+        }
+      // });
+    }
+
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end();
+  }
+});
+
+server.listen(3000,'localhost',()=>{
+  console.log('3000port');
+})
+
