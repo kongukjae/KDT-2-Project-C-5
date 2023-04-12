d84611535f449f8d026e2562c93d73f6c7653384
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-12 16:48:07 +0900

    로컬환경 앱서버 구축 server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((request, response)=> {
+  if(request.method === 'GET'){
+    if(request.url === '/'){
+      response.writeHead(200, {'Content-type':'text/HTML'});
+      response.write(fs.readFileSync('./first.html','utf8'));
+      response.end;
+    }
+  }
+})
+server.listen(3000, err=>{
+  if(err){
+    console.error(`에러, (${err})가 발생했습니다.`);
+  } else {
+    console.log("포트 3000으로 연결합니다.")
+  }
+})
