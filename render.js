06c23e449c49c6b9cc8338ebfef3347dd4fc2868
jieun <je_eun2735@naver.com>
2023-04-13 12:11:53 +0900

    render.js 최초 진입 파일

--- render.js
+++ render.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./app";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.ReactDOM.render(
+  //컴포넌트 위치 root
+  //App.js에서 만든 Component를 띄운다.
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
