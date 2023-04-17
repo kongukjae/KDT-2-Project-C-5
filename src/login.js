bed6eed18427c160a11c935d038c166f64365f0a
leejaekwon <nowkwon97@gmail.com>
2023-04-17 16:43:47 +0900

    로그인 페이지 초안 완성

--- src/login.js
+++ src/login.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 const login = () => {
+  const inputStyle = {
+    width: '150%',
+    height: '54px',
+    backgroundColor: '#D9D9D9',
+    border: '1px solid #D9D9D9',
+    borderRadius: '20px',
+    textAlign: 'center'
+  }
+
   return (
     <div id='login-page'>
       <div>BOOK STAGRAM</div>
       <div>
         <form>
-          <p>ID</p>
-          <input type="text" placeholder='ID를 입력하세요' name='id'></input>
-          <p>password</p>
-          <input type="password" placeholder='password를 입력하세요' name='password'></input>
+          <p><b>ID</b></p>
+          <div><input type="text" placeholder='ID를 입력하세요' name='id' style={inputStyle}></input></div>
+          <p><b>password</b></p>
+          <div><input type="password" placeholder='password를 입력하세요' name='password' style={inputStyle}></input></div>
         </form>
       </div>
       <div>login</div>
