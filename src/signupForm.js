c317469ab61b3be9bf00665f60f6dcead2739752
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-19 16:24:22 +0900

    테스트용 html 및 js파일 src에 생성.

---
+++ src/signupForm.js
@@ -0,0 +1,15 @@
+function signupForm(){
+  return 
+  `<h1>북스타그램</h1>
+  <form>
+    <label>
+      ID:
+      <input type="text" name="id" />
+    </label>
+    <label>
+      Password:
+      <input type="password" name="password" />
+    </label>
+    <button type="submit">로그인</button>
+  </form>`
+}
