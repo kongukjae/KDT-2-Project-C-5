47e108d5925fd4f06f48b911ed06b983eec0f846
jungjieun <je_eun2735@naver.com>
2023-04-19 11:15:33 +0900

    클릭이벤트 만들기

--- test/src/App.js
+++ test/src/App.js
@@ -4,9 +4,15 @@ import LogoPage from "./LogoPage";
 import LoginPage from "./LoginPage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // LoginPage 또는 logoPage를 렌더링
   return (
     <div className="App">
-      <LogoPage />
+      {isLoggedIn ? (
+        <LogoPage setIsLoggedIn={setIsLoggedIn} />
+      ) : (
+        <LoginPage setIsLoggedIn={setIsLoggedIn} />
+      )}
     </div>
   );
 }
