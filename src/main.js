666e15048b01a63bb52deb40bc20573b66b017c6
leejaekwon <nowkwon97@gmail.com>
2023-04-19 11:51:38 +0900

    라우터 첫 시도 결과 에러 폭탄

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import StartPage from './startScreen';
 import LoginPage from './login';
@@ -7,7 +8,13 @@ import './index.css'
 
 
 const App = () => {
-  return <LoginPage/>;
+  // return <LoginPage/>;
+  return (
+    <Router>
+      <Route path='/' component={StartPage} />
+      <Route path='/login' component={LoginPage} />
+    </Router>
+  )
 };
 
 createRoot(document.getElementById('root')).render(<App />);
