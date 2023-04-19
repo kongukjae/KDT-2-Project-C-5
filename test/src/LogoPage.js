0c4c071dc37f186fca08e015b8300bf0a203635d
jungjieun <je_eun2735@naver.com>
2023-04-19 10:56:27 +0900

    LoginPage.js파일 생성

--- test/src/LogoPage.js
+++ test/src/LogoPage.js
@@ -1,2 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
+
+function LogoPage() {
+  return (
+    <div className="logo-page">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+  );
+}
+
+export default LogoPage;
