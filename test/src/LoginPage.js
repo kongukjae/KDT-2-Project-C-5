06344990aeabc995bff9e0b15e2d07c821f4bf92
jungjieun <je_eun2735@naver.com>
2023-04-19 17:21:56 +0900

    useHistory

--- test/src/LoginPage.js
+++ test/src/LoginPage.js
@@ -16,10 +16,13 @@ const LoginPage = () => {
     event.preventDefault();
     console.log(`ID: ${id}, Password: ${password}`);
   };
+  //로그인 버튼 클릭 시 호출되는 기능이다. 기본 양식 제출 동작을 방지하고 이메일과 암호를 콘솔에 기록.
 
   const handleGoogleLogin = () => {
     console.log("Google login clicked");
   };
+  //Google 함수 처리 handleGoogleLogin Google 로그인 버튼을 클릭하면 함수가 호출된다.
+  // 새 탭에서 Google 로그인 페이지가 열림.
 
   const handleAppleLogin = () => {
     console.log("Apple login clicked");
