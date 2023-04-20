29623d1bc84d4f27d1691324d11b38021ba775f2
wjkk21 <verstand821@gmail.com>
2023-04-20 16:47:54 +0900

    페이지만듬

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ import React from "react";
 export default function App(){
 
     return(
+        <div>
+            <header>
+            <h1>북스타그램</h1>
+            </header>
+        <main>
+    
+            <h2>Login</h2>
+    
+            <form>
+            <input type="text" name="userName" placeholder="Email" />
+            <input type="password" name="userPassword" placeholder="Password" />
         
-
+            <input type="submit" value="Login" />
+            </form>
+        </main>
+        <footer>
+            
+        </footer>
+      </div>
     )
 }
