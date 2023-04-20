751781b18698871209451cf3d5ba05a69d62aae5
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-20 16:12:47 +0900

    로그인 form을 App()에 작성

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ import React from "react";
 export default function App(){
 
     return(
-        
-
+        <div>
+            <h1>북스타그램</h1>
+            <form method="GET" action="/login">
+                <label>
+                    ID:
+                    <input type="text" name="id" />
+                </label>
+                <label>
+                Password:
+                    <input type="password" name="password" />
+                </label>
+            <button type="submit">로그인</button>
+            </form>
+        </div>
     )
 }
