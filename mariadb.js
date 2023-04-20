85e892ca017a293540210a780e9d9ff09ef34974
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-20 11:30:51 +0900

    mariadb.js readFileSync가 아닌 readFile로 시도. 적용안됨.

--- mariadb.js
+++ mariadb.js
@@ -1,12 +1,23 @@
 import mariadb from 'mariadb';
 import fs from 'fs';
 import CRUD from './signUpCRUD.js';
-import poolSet from './dist/SQL.json' assert {type: "json"};
+//실험적 기능이기에 fs을 사용하기로 함. 
+//import poolSet from './dist/SQL' assert {type: "json"};
 
-//밖으로 옮겨 모듈화 작업을 시작함.
+//모듈화 작업을 시작함.
 export default async function SQLConnection() {
-  //createPool - 서버연동, 이것이 없으면 DB에 접근이 불가능하다.
   
+  // dist의 SQL에서 mariadb용 pool 세팅을 가져옴.
+  const poolSet = "";
+  fs.readFile("./dist/SQL.json", "utf8",(err,data)=>{
+    if(err){
+      console.error(`${err}가 발생했습니다.`, err);
+    } else {
+      poolSet = data;
+    }
+  });
+  console.log(poolSet);
+  // createPool - 서버연동, 이것이 없으면 DB에 접근이 불가능하다.
   const pool = mariadb.createPool(poolSet.mariadb);
   let conn;
   try {
@@ -22,6 +33,10 @@ export default async function SQLConnection() {
     } 
   }
 }
-//모듈 signupCRUD CRUD로 저장후, 적용되는지 테스트
-console.log(CRUD.createUser("dgchoi", "abc123", "dgchoi@email.com", "dgchoi"));
-console.log(poolSet);
+// 모듈 signupCRUD CRUD로 저장후, 적용되는지 테스트
+//console.log(CRUD.createUser("dgchoi", "abc123", "dgchoi@email.com", "dgchoi"));
+
+// Pool 세팅 SQL로 옮기고 fs로 파일 읽어오기. 되는지 테스트
+//console.log(poolSet);
+
+SQLConnection();
