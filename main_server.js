c71f5ae0c42ee2ea561437273437e69536d9a2c1
AmondChocolateShake <dongju230130@gmail.com>
2023-04-20 14:19:44 +0900

    standard setting

--- main_server.js
+++ main_server.js
@@ -1,9 +1,20 @@
-const http=require('http');
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((request, response) => {
+    const file = fs.readFileSync("./index.html", "utf8");
+    const jsFile=fs.readFileSync("./render.js","utf8");
+    if (request.method === "GET") {
+    
+    }
+    
+});
 
 
-const server=http.createServer(function(req,res){
-  res.writeHead()
-  
-
-})
-
+server.listen(3000, (err) => {
+    if (err) {
+    console.error(`에러, (${err})가 발생했습니다.`);
+    } else {
+    console.log("포트 3000으로 연결합니다.");
+    }
+});
