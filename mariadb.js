ce56f8b709572fb7a05e124b4832d3e6cb14d6e1
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-20 10:49:40 +0900

    mariaDB 모듈화 고려한 세팅 및 회원가입 CRUD 모듈 적용 테스트

--- mariadb.js
+++ mariadb.js
@@ -1,11 +1,11 @@
 import mariadb from 'mariadb';
 import fs from 'fs';
-//createPool - 서버연동, 이것이 없으면 DB에 접근이 불가능하다.
-const pool = mariadb.createPool({host :"localhost", user: "bookstargram", password:'qwe123', port:"3306", database:'bookstargram', connectionLimit: 5});
-//
-async function SQLConnection() {
+import CRUD from './signUpCRUD.js';
+
+export default async function SQLConnection() {
+  //createPool - 서버연동, 이것이 없으면 DB에 접근이 불가능하다.
+  const pool = mariadb.createPool({host :"localhost", user: "bookstargram", password:'qwe123', port:"3306", database:'bookstargram', connectionLimit: 5});
   let conn;
-  
   try {
     conn = await pool.getConnection();
     //await conn.query(`insert into htmltest(html_text) values ('${htmldata}')`);
@@ -19,4 +19,5 @@ async function SQLConnection() {
     } 
   }
 }
-SQLConnection();
+//모듈 signupCRUD CRUD로 저장후, 적용되는지 테스트
+console.log(CRUD.createUser("dgchoi", "abc123", "dgchoi@email.com", "dgchoi"));
