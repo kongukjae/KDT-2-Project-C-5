509889a797365a0f8a1fb5b91f1a75ed42ba15b2
jungjieun <je_eun2735@naver.com>
2023-04-20 09:49:47 +0900

    App.js이벤트연습

--- test/src/App.js
+++ test/src/App.js
@@ -5,11 +5,12 @@ import LoginPage from "./LoginPage";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-
+  //useState 훅을 사용해서 loggedin 상태를 관리한다.
+  //loggedIn은 사용자가 로그인하면 true로 설정되고, 그렇지 않으면 false로 유지된다.
   const handleLogin = () => {
     setLoggedIn(true);
   };
-
+  //handleLogin 함수는 loggedIn 상태를 업데이트하는 데 사용. 클릭될 때 호출된다.
   return (
     <div className="App">
       {loggedIn ? (
@@ -22,5 +23,8 @@ function App() {
     </div>
   );
 }
+//컴포넌트는 loggedIn 상태에 따라 렌더링됨.
+//만약 loggedIn이 true이면 LoginPage 컴포넌트가 렌더링되며, handleLogin 함수가 전달
+//만약 loggedIn이 false이면 LogoPage 컴포넌트가 렌더링, 로고를 클릭하면 handleLogin 함수가 호출
 
 export default App;
