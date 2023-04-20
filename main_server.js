880f320d0226ebcddb3066c7cb8f251695ab4273
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-20 15:25:39 +0900

    3. 로그인 기능 Fixes #9 앱서버에서 index.html 응답 설정

--- main_server.js
+++ main_server.js
@@ -5,15 +5,27 @@ const server = http.createServer((request, response) => {
     const file = fs.readFileSync("./index.html", "utf8");
     const jsFile=fs.readFileSync("./render.js","utf8");
     if (request.method === "GET") {
-    
+        if(req.url === "/"){
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(file);
+            res.end();
+            
+        }
+        if(req.url === "/render.js"){
+            res.writeHead(200, {'Content-Type':'text/javascript'});
+            res.write(jsFile);
+            res.end();
+        }
     }
-    
+    if(req.method === "POST"){
+
+    }
 });
 
 
 server.listen(3000, (err) => {
     if (err) {
-    console.error(`에러, (${err})가 발생했습니다.`);
+    console.error(`에러, (${err})가 발생했습니다.`, err);
     } else {
     console.log("포트 3000으로 연결합니다.");
     }
