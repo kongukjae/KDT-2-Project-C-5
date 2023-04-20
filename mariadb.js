94b16d9eb40afba93044b6b2028a93cef5fd342d
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-20 14:45:55 +0900

    export하는 북스타그램 DB로 쿼리 보내는 함수 설명 첨부

--- mariadb.js
+++ mariadb.js
@@ -5,7 +5,11 @@ import fs from 'fs';
 //import poolSet from './dist/SQL' assert {type: "json"};
 
 //모듈화 작업을 시작함.
-export default async function bookstargramConnect(func) {
+/**
+ * 마리아DB 모듈을 사용하여 쿼리를 보내는 모듈. 호출시 설정한 데이터베이스에 pool을 연결하고 DB에 query문을 한번 보내고 res에 그 응답을 받는다. 이후 release()및 end()로 연결을 종료한다.
+ * @param query DB로 보낼 쿼리문, 문자열.
+ */
+export default async function bookstargramConnect(query) {
   
   // dist의 SQL에서 mariadb용 pool 세팅을 가져옴.
   const poolSet = JSON.parse(fs.readFileSync("./dist/SQL.json", "utf8"));
@@ -16,8 +20,9 @@ export default async function bookstargramConnect(func) {
   try {
     conn = await pool.getConnection();
     //await conn.query(`insert into htmltest(html_text) values ('${htmldata}')`);
-    const res = await conn.query(func);
-    console.log(res);
+    const res = await conn.query(query);
+    return res;
+    //console.log(res);
 
   } finally {
     if (conn){
@@ -26,6 +31,7 @@ export default async function bookstargramConnect(func) {
     } 
   }
 }
+
 // 모듈 signupCRUD CRUD로 저장후, 적용되는지 테스트
 //console.log(CRUD.createUser("dgchoi", "abc123", "dgchoi@email.com", "dgchoi"));
 
