870def7ea82ad4750aa2599bd918400dd046a9b2
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-20 11:04:39 +0900

    Pool 생성문 옵션 리터럴 수정, SQL.json을 생성후 파일을 읽는식으로 전환

--- mariadb.js
+++ mariadb.js
@@ -1,11 +1,13 @@
 import mariadb from 'mariadb';
 import fs from 'fs';
 import CRUD from './signUpCRUD.js';
+import poolSet from './dist/SQL.json' assert {type: "json"};
 
 //밖으로 옮겨 모듈화 작업을 시작함.
 export default async function SQLConnection() {
   //createPool - 서버연동, 이것이 없으면 DB에 접근이 불가능하다.
-  const pool = mariadb.createPool({host :"localhost", user: "bookstargram", password:'qwe123', port:"3306", database:'bookstargram', connectionLimit: 5});
+  
+  const pool = mariadb.createPool(poolSet.mariadb);
   let conn;
   try {
     conn = await pool.getConnection();
@@ -22,3 +24,4 @@ export default async function SQLConnection() {
 }
 //모듈 signupCRUD CRUD로 저장후, 적용되는지 테스트
 console.log(CRUD.createUser("dgchoi", "abc123", "dgchoi@email.com", "dgchoi"));
+console.log(poolSet);
