7fd30dd9c5205db6629a092cef37c8c5aedb2a9d
jungjieun <je_eun2735@naver.com>
2023-04-21 12:54:58 +0900

    App.js컴포넌트를 index.js에서 랜더링

---
+++ 3.react/src/index.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// // React 컴포넌트를 작성
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello, React!</h1>
+//     </div>
+//   );
+// }
+
+// // ReactDOM.render()를 사용하여 React 컴포넌트를 렌더링
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
