579751d20a81307539e815da74e11b849c39cf9a
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-21 11:14:07 +0900

    3. 로그인 기능 Fixes #9 React에 맞게 form submit 조정중. onSubmit 이벤트를 캐치할 때 함수 실행하게 함.

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 import React from "react";
 
 export default function App(){
+    function handleSubmit(event) {
+        //기본 공백이 입력되는걸 방지하기 위한 구문.
+        event.preventDefault();
+
+        //받은 이벤트의 타겟, submit된 데이터(id, password)를 form으로 저장하는 구문
+        const form = event.target;
+        fetch()
+    }
 
     return(
         <div>
             <h1>북스타그램</h1>
-            <form method="GET" action="/login">
+            <form method="POST" onSubmit={handleSubmit}>
                 <label>
                     ID:
-                    <input type="text" name="id" />
+                    <input type="text" name="id" placeholder="ID를 입력하세요" />
                 </label>
                 <label>
                 Password:
-                    <input type="password" name="password" />
+                    <input type="password" name="password" placeholder="패스워드를 입력하세요" />
                 </label>
             <button type="submit">로그인</button>
             </form>
