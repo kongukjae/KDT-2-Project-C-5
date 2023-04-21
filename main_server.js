123750dd5fff6d5134fd3f008eadac514953fde0
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-21 13:10:44 +0900

    formidable 모듈 설치 및 로그인 폼 parse 적용

--- main_server.js
+++ main_server.js
@@ -1,6 +1,6 @@
 import http from "http";
 import fs from "fs";
-import queryString from 'querystring';
+import formidable from 'formidable';
 
 const server = http.createServer((request, response) => {
     const file = fs.readFileSync("./index.html", "utf8");
@@ -21,14 +21,20 @@ const server = http.createServer((request, response) => {
     }
     if(request.method === "POST"){
         if(request.url === "/login"){
+            //기본 요청의 body 데이터 가져올때의 구문.
             request.on('data', loginForm=>{
-                for(let array in loginForm.entries()){
-                    console.log(array[0]);
-                    console.log(array[1]);
-                }
-                //console.log(data.password);
-
+                let formdata = loginForm.toString();
+                
+                console.log(formdata);
             })
+            //Multipart Formdata를 자체 해석하여 파싱해주는 모듈. 아래와같이 옵션을 추가하고 parse할때 파싱할 request와 err, fields를 콜백함수 매개변수로 두고 콜백에서 다룰 수 있게 해준다.
+            formidable({multiples: true}).parse(request, (err, fields)=> {
+                    if(err){
+                        console.error(err, err);
+                        return;
+                    }
+                    console.log(fields);
+                })
         }
     }
 });
