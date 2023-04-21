1de087c426d8ca68ef6e91846d78fe30f2d66364
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-21 15:17:29 +0900

    2. 로그인 페이지 Fixes #8 로그인 컴포넌트 추가.

---
+++ src/login.js
@@ -0,0 +1,29 @@
+import React from "react";
+export default function Login(){
+    //form이 submit되는 이벤트를 캐치, 이때 해당 이벤트를 event 매개변수로 받고 진행함.
+    function handleSubmit(event) {
+      //새창을 여는 기본 설정을 방지하는 구문.
+      event.preventDefault();
+
+      //받은 이벤트의 타겟, submit된 데이터(id, password)를 form으로 저장하는 구문
+      const form = event.target;
+      //해당 타겟을 FormData 형식으로 변환, 저장하는 구문.
+      const formData = new FormData(form);
+      //fetch로 서버에 데이터를 보내는 구문.
+      fetch("/login", {method: form.method, body: formData})
+  }  
+  return(
+    <form method="POST" onSubmit={handleSubmit}>
+      <label>
+        ID:
+        <input type="text" name="id" placeholder="ID를 입력하세요" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" placeholder="패스워드를 입력하세요" />
+      </label>
+      <button type="submit">로그인</button>
+    </form>
+
+  )
+}
