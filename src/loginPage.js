b50d194dd8214175be96c8a703cfe2b54ab28584
leejaekwon <nowkwon97@gmail.com>
2023-04-21 14:35:28 +0900

    .jsx .js 파일 테스트

--- src/loginPage.js
+++ src/loginPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BookStagramTopLogo from '../components/BookStagramTopLogo.jsx';
+import BookStagramTopLogo from '../components/BookStagramTopLogo';
 import InputId from '../components/InputId';
 import InputPwd from '../components/InputPwd';
 import ButtonLogin from '../components/ButtonLogin';
