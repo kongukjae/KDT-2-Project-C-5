9fccab5698ae32cf4c7ee06c88b632c70b9a9047
leejaekwon <nowkwon97@gmail.com>
2023-04-24 11:06:08 +0900

    pwd check 컴포넌트 생성

---
+++ components/InputPwdCheck.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// 비밀번호 확인을 위해 비밀번호 입력 컴포넌트를 import 
+import InputPwd from './InputPwd';
+
+const InputPwdCheck = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default InputPwdCheck;
