50aad29441fc2a538702aa4dff319830a7e257c4
jieun <je_eun2735@naver.com>
2023-04-24 13:04:14 +0900

    error test branch

--- 3.react/src/server.js
+++ 3.react/src/server.js
@@ -41,3 +41,31 @@
 // server.listen(3000, () => {
 //   console.log("Server is listening on port 3000");
 // });
+
+//404error 경로 확인 중
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    const filePath = path.join(__dirname, "public", "index.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
