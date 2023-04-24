76392f5cf8d45d386e0ba426f510b4ab5577e7bd
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-24 17:19:15 +0900

    쓰지않는 useState 삭제, 번들링 작업후 작동하는 것 확인

--- components/tag.js
+++ components/tag.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Tag(props) {
-  props [tagName, setTagName] = useState(props.name)
   return(
     <label>
       <input type="checkbox"name={props.name} /> {props.name}
