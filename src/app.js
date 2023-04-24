30977fae3f1632058d176063ec6625593eccf051
AmondChocolateShake <dongju230130@gmail.com>
2023-04-24 16:58:15 +0900

    fixed and build successed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
 
-function SearchBox({ onSearch }) {
-  const [query, setQuery] = useState("");
-
-  return <h1>hello</h1>;
+    return(
+        <h1>hello</h1>
+    )
 }
