f81b601cd63157b178b82eb3928992f8411d6ea6
jungjieun <je_eun2735@naver.com>
2023-04-25 14:31:34 +0900

    검색,피드작성 폼작성

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
@@ -15,6 +16,23 @@ import './index.css';
 
 const App = () => {
   return <LoginPage/>;
+=======
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import StartPage from "./startScreen";
+import Login from "./login";
+import LoginPage from "./loginPage";
+import SignUpPage from "./signUp";
+import Router from "./router";
+import MainFeed from "./mainFeed";
+import NavBar from "../components/NavBar";
+import "./index.css";
+import ReviewPage from "./ReviewPage";
+
+const App = () => {
+  return <ReviewPage />;
+>>>>>>> 6de1ff8d (검색,피드작성 폼작성)
 };
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
