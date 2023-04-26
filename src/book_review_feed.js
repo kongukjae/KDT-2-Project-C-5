d25313c1ee1460d18fb93c3319ace0bb0f2e9c3a
jieun <je_eun2735@naver.com>
2023-04-26 17:08:23 +0900

    현재 업로드 시간을 구할 수 없어서 현재시각으로 대신함

--- src/book_review_feed.js
+++ src/book_review_feed.js
@@ -2,14 +2,14 @@ import React from "react";
 import Bookinfo from "../components/Bookinfo";
 import BookDescription from "../components/BookDescription";
 import FeedTopBar from "../components/FeedTopBar";
+import UploadTime from "../components/UploadTime";
 
 const Book_review_feed = () => {
   return (
     <div>
       <FeedTopBar />
       <Bookinfo />
-      <div>11view</div>
-      <div>1시간 전</div>
+      <UploadTime />
       <BookDescription />
     </div>
   );
