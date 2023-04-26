810e9c73f7e0b4b1f3065c3c0c2ab5bb94843092
jieun <je_eun2735@naver.com>
2023-04-26 11:44:50 +0900

    API_URL?query=

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -56,14 +56,19 @@ function Autocomplete(props) {
 
   useEffect(() => {
     // 검색어에 대한 결과를 가져오는 함수
+    // const fetchSearchResults = async () => {
+    //   const response = await fetch(`API_URL?query=${value}`);
+    //   const results = await response.json();
+    //   setSearchResults(results);
+    // };
     const fetchSearchResults = async () => {
       const response = await fetch(`API_URL?query=${value}`);
       const results = await response.json();
       setSearchResults(results);
+      console.log(results);
     };
 
     // fetchSearchResults() 함수를 호출하여 검색 결과를 가져와 setSearchResults() 함수를 사용하여 searchResults 상태 변수를 업데이트
-
     if (value !== "") {
       fetchSearchResults();
     } else {
@@ -72,10 +77,8 @@ function Autocomplete(props) {
   }, [value]);
   console.log(searchResults);
 
-  //색어가 빈 문자열일 경우, searchResults 상태 변수를 빈 배열([])로 업데이트
   //검색창과 검색 결과 목록
   //검색버튼
-  //searchResults 배열을 map() 함수를 사용하여 순회하면서 검색 결과를 <li> 태그로 감싸 출력
   return (
     <form onSubmit={onSubmit}>
       <input type="text" value={value} onChange={onChange} />
