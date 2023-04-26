2f08bc7424eac754a06f5bd21aed0d5ab7f6c0cd
jieun <je_eun2735@naver.com>
2023-04-26 16:58:46 +0900

    FeedTopBar 사진 ,이름,구독중,알람

--- components/Bookinfo.js
+++ components/Bookinfo.js
@@ -1,14 +1,18 @@
 import React from "react";
 
+const InfoStyle = {
+  display: "flex",
+  flexDirection: "row",
+};
 const Bookinfo = () => {
   return (
     <div>
       <br />
       <div>책사진</div>
-      <div>책 이름:우주와 달</div>
-      <div>지은이:정지은</div>
-      <div>발행처:교보</div>
-      <div>페이지수:526P</div>
+      <div style={InfoStyle}>책 이름:우주와 달</div>
+      <div style={InfoStyle}>지은이:정지은</div>
+      <div style={InfoStyle}>발행처:교보</div>
+      <div style={InfoStyle}>페이지수:526P</div>
     </div>
   );
 };
