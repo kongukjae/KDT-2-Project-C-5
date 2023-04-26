49d0946f2f5463105035f428f1f7aea6c3ae5775
leejaekwon <nowkwon97@gmail.com>
2023-04-26 09:22:11 +0900

    알람 폼 제작

---
+++ src/mainFeedReviewForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const mainFeedReviewForm = () => {
+  return (
+    <div>
+      <div>
+        책 표지 부분
+      </div>
+      <div>
+        사진, 닉네임, 3줄리뷰 부분
+      </div>
+    </div>
+  );
+};
+
+export default mainFeedReviewForm;
