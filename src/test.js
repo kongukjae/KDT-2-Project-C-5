d7a7504955063dbc7d11d44eb806d7355632d13a
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-26 17:14:54 +0900

    path 사용하여 절대경로, 파일, 확장자 추출

--- src/test.js
+++ src/test.js
@@ -1,16 +1,24 @@
-import http from 'http';
-import fs from 'fs';
-import url, { pathToFileURL } from 'url';
-import path from 'path';
+  import http from 'http';
+  import fs from 'fs';
+  import url, { pathToFileURL } from 'url';
+  import path from 'path';
 
-//console.log(path.__dirname)
-//console.log(path.__filename);
-//const fileURLone = pathToFileURL("./");
-//console.log(fileURLone);
-//console.log(import.meta.url);
-//console.log(fileURLone.toString());
-const testOne = url.parse("D:/dgchoi/KDT-2-Project-C-5/src/../dist/main.js");
-console.log(testOne.pathname)
-console.log(testOne);
-const testCode = fs.readFileSync(testOne.pathname);
-console.log(testCode.toString("utf8"));
+  //console.log(path.__dirname)
+  //console.log(path.__filename);
+  //const fileURLone = pathToFileURL("./");
+  //console.log(fileURLone);
+  //console.log(import.meta.url);
+  //console.log(fileURLone.toString());
+  const testOne = url.parse("D:/dgchoi/KDT-2-Project-C-5/src/../dist/main.js");
+  console.log(testOne.pathname)
+  //상대경로를 쓴 것을 단순 절대경로로 치환해주는 함수 사용, path.resolve();
+  console.log(path.resolve(testOne.pathname));
+  //resolve로 절대경로로 변경한 것의 디렉토리를 출력하는 함수, dirname()
+  console.log(path.dirname(path.resolve(testOne.pathname)));
+  //testOne에 지정된 url의 basename, 파일 이름을 parse하여 출력하는 함수 basename();
+  console.log(path.basename(testOne.pathname))
+  //path.parse - 지정된 경로를 하나의 객체로 변환하는 함수. root와 dir, base, ext, name을 가진다.
+  console.log(path.parse(import.meta.url));
+  const testCode = fs.readFileSync(path.join(testOne.pathname));
+  console.log(testCode.toString("utf8"));
+  //console.log(testCode)
