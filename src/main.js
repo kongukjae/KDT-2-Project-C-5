c2d7fe97983240b3555d525781a48b09e41533d9
leejaekwon <nowkwon97@gmail.com>
2023-04-26 11:25:38 +0900

    컴포넌트 정규화 작업을 위한 작업

--- src/main.js
+++ src/main.js
@@ -9,11 +9,26 @@ import Router from './router';
 import MainFeed from './mainFeed';
 import NavBar from '../components/NavBar';
 import MainFeedReviewForm from './mainFeedReviewForm';
+import BookStagramTopLogo from '../components/BookStagramTopLogo';
+import ButtonLogin from '../components/ButtonLogin';
+import ButtonSignUp from '../components/ButtonSignUp';
+import ButtonSignUpWithAppleAccount from '../components/ButtonSignUpWithAppleAccount';
+import ButtonSignUpWithGoogle from '../components/ButtonSignUpWithGoogle';
+import ButtonSubmit from '../components/ButtonSubmit';
+import ForgotAccount from '../components/ForgotAccount'
+import Hamburger from '../components/Hamburger'
+import InputEmail from '../components/InputEmail';
+import InputId from '../components/InputId';
+import InputProfilePic from '../components/InputProfilePic';
+import InputPwd from '../components/InputPwd';
+import InputPwdCheck from '../components/InputPwdCheck';
+import InputUserName from '../components/InputUserName';
+import MainFeedContents from '../components/MainFeedContents';
 import './index.css';
 
 
 const App = () => {
-  return <MainFeedReviewForm/>;
+  return <SignUpPage/>;
 };
 
 createRoot(document.getElementById('root')).render(<App />);
--- src/mainFeedReviewForm.js
+++ src/mainFeedReviewForm.js
@@ -12,11 +12,12 @@ const mainFeedReviewForm = () => {
   return (
     <div style={conStyle}>
       <div>
-        책 표지 부분
+        책 표지
       </div>
       <div>
         <div>사진, 닉네임</div>
-        <div>3줄리뷰 부분</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin arcu metus, posuere
+        </div>
       </div>
     </div>
   );
