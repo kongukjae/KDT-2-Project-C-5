475c375e3fe35c69bdf4748af70604d013e5824e
leejaekwon <nowkwon97@gmail.com>
2023-04-26 16:20:49 +0900

    취소버튼 컴포넌트 추가

---
+++ components/ButtonCancle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonCancle = () => {
+  return (
+    <div>
+      <Button>취소</Button>
+    </div>
+  );
+};
+
+export default ButtonCancle;
--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ import Router from './router';
 import MainFeed from './mainFeed';
 import NavBar from '../components/NavBar';
 import MainFeedReviewForm from './mainFeedReviewForm';
+import ButtonCancle from '../components/ButtonCancle';
 import './index.css';
 
 
 const App = () => {
-  return <MainFeedReviewForm/>;
+  return <ButtonCancle/>;
 };
 
 createRoot(document.getElementById('root')).render(<App />);
