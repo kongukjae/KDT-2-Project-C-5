ee7039ce72ece697b6d30f1a85fe346b38157b1a
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-26 18:07:04 +0900

    팀원이 만든 컴포넌트들을 추가하여 사용함. 설명은 컴포넌트 정규화 작업 보드 확인

--- components/LongReview.js
+++ components/LongReview.js
@@ -1,37 +1,31 @@
 import React from "react";
-
-export default function App() {
+// 사용하는 컴포넌트들
+// 사용자 정보
+import FeedTopBar from "../components/FeedTopBar.js"
+// 책 정보
+import BookInfo from "../components/BookInfo.js"
+// 긴 리뷰글
+import UploadTime from "../components/UploadTime.js"
+export default function LongReview() {
 
   return (
-    //전체 컨테이너
     <div>
-      <div>
-        <p>사용자 정보 컨테이너</p>
-
-        <div>"사용자 프로필 이미지"</div>
-        <div>사용자 이름</div>
-        <div>알림 아이콘</div>
-      </div>
-      <div>
-        <p>책정보 컨테이너</p>
-
-        <div>"책 커버 이미지"</div>
-        <div>책 기본정보 컨테이너</div>
-      </div>
+      <FeedTopBar />
+      <BookInfo />
       <div>
         <p>긴 리뷰글 컨테이너</p>
+
         <div>
           <span>조회기록</span>
-          <span>작성시간</span>
+          <UploadTime />
         </div>
 
         <div>사용자 리뷰</div>
-        <button>저장 또는 공유하기</button>
+        
         <div>
-          <p>좋아요 컨테이너</p>
-          
-          <div>좋아요 갯수</div>
-          <div>아이콘</div>
+          <button>저장 또는 공유하기</button>
+          <span>좋아요 갯수</span>
+          <span>아이콘</span>
         </div>
       </div>
     </div>
