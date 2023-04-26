a9220c2c9bbf9e722897809961a686cd0b944dc9
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-26 16:15:35 +0900

    절대경로에 상대경로를 붙여 사용할 수 있는가 테스트

--- src/test.js
+++ src/test.js
@@ -3,8 +3,14 @@ import fs from 'fs';
 import url, { pathToFileURL } from 'url';
 import path from 'path';
 
-console.log(path.__dirname)
-console.log(path.__filename);
-const fileURLone = pathToFileURL("./");
-console.log(fileURLone);
-console.log(import.meta.url);
+//console.log(path.__dirname)
+//console.log(path.__filename);
+//const fileURLone = pathToFileURL("./");
+//console.log(fileURLone);
+//console.log(import.meta.url);
+//console.log(fileURLone.toString());
+const testOne = url.parse("D:/dgchoi/KDT-2-Project-C-5/src/../dist/main.js");
+console.log(testOne.pathname)
+console.log(testOne);
+const testCode = fs.readFileSync(testOne.pathname);
+console.log(testCode.toString("utf8"));
