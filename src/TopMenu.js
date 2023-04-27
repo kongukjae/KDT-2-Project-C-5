d869539cd7580e48a3cfef38bb5732a3904d242e
jieun <je_eun2735@naver.com>
2023-04-27 11:31:43 +0900

    버튼 클릭시 alert에서 페이지 전환으로 바꾸어야 합니다!

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -4,13 +4,13 @@ import HambergerMenu from "../components/HambergerMenu";
 
 const TopMenu = () => {
   const menuItems = [
-    { label: "계정설정", onClick: () => alert("메뉴1 클릭됨") },
-    { label: "알림설정", onClick: () => alert("메뉴2 클릭됨") },
-    { label: "관심친구", onClick: () => alert("메뉴3 클릭됨") },
-    { label: "태그설정", onClick: () => alert("메뉴1 클릭됨") },
-    { label: "차단목록", onClick: () => alert("메뉴1 클릭됨") },
-    { label: "구매하기", onClick: () => alert("메뉴1 클릭됨") },
-    { label: "로그아웃", onClick: () => alert("메뉴1 클릭됨") },
+    { label: "계정설정", onClick: () => alert("페이지전환") },
+    { label: "알림설정", onClick: () => alert("페이지전환") },
+    { label: "관심친구", onClick: () => alert("페이지전환") },
+    { label: "태그설정", onClick: () => alert("페이지전환") },
+    { label: "차단목록", onClick: () => alert("페이지전환") },
+    { label: "구매하기", onClick: () => alert("페이지전환") },
+    { label: "로그아웃", onClick: () => alert("페이지전환") },
   ];
 
   return (
