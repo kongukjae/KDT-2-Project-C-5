85e8a83ab9985f085f9c3120e2648e88be258696
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-27 12:49:50 +0900

    test_DBConnect 원상태로 복구, 테스트한 코드 파일로 따로 분리

--- src/test_DBConnect.js
+++ src/test_DBConnect.js
@@ -1,15 +1,8 @@
 import bookstargramConnect from './mariadb.js';
 import signUpQuery from './signUpQuery.js'
-import path from "path";
-import url from "url";
-const currentPath = url.fileURLToPath(import.meta.url);
-const rootPath = path.normalize(currentPath + "\\..")
 
-console.log(path.join(rootPath, "dist/root.json"));
-console.log(rootPath);
-console.log(currentPath);
-// bookstargramConnect(signUpQuery.createUser("human47", "abc47", "human47@email.net","휴먼47"))
-// .then(result=>{
-//   console.log(result);
-//   process.exit()
-// })
+bookstargramConnect(signUpQuery.createUser("human47", "abc47", "human47@email.net","휴먼47"))
+.then(result=>{
+  console.log(result);
+  process.exit()
+})
