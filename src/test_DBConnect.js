216a13706d11e2aaecc778571101984b67f216c5
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-27 11:13:00 +0900

    테스트용 커밋에 readAll이 아닌 createUser() 함수를 호출

--- src/test_DBConnect.js
+++ src/test_DBConnect.js
@@ -1,7 +1,7 @@
 import bookstargramConnect from './mariadb.js';
 import signUpQuery from './signUpQuery.js'
 
-bookstargramConnect(signUpQuery.readAll())
+bookstargramConnect(signUpQuery.createUser("human47", "abc47", "human47@email.net","휴먼47"))
 .then(result=>{
   console.log(result);
   process.exit()
