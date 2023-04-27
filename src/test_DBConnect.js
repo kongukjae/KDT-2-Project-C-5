3c0aec83c54f239b8f92ea6682bc7ceac3747934
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-27 12:46:58 +0900

    앤트리 파일 기준으로 root 경로 설정하기 + 설정된 root에 다른 경로 붙여보기

--- src/test_DBConnect.js
+++ src/test_DBConnect.js
@@ -1,8 +1,15 @@
 import bookstargramConnect from './mariadb.js';
 import signUpQuery from './signUpQuery.js'
+import path from "path";
+import url from "url";
+const currentPath = url.fileURLToPath(import.meta.url);
+const rootPath = path.normalize(currentPath + "\\..")
 
-bookstargramConnect(signUpQuery.createUser("human47", "abc47", "human47@email.net","휴먼47"))
-.then(result=>{
-  console.log(result);
-  process.exit()
-})
+console.log(path.join(rootPath, "dist/root.json"));
+console.log(rootPath);
+console.log(currentPath);
+// bookstargramConnect(signUpQuery.createUser("human47", "abc47", "human47@email.net","휴먼47"))
+// .then(result=>{
+//   console.log(result);
+//   process.exit()
+// })
