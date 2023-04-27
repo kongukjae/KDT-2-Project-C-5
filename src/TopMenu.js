7960c64923b88dd70586d1e9b4925deb6bb7fb08
jieun <je_eun2735@naver.com>
2023-04-27 11:30:21 +0900

    menuItems 변경

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -1,12 +1,23 @@
 import React from "react";
 import BookStagramTopLogo from "../components/BookStagramTopLogo";
+import HambergerMenu from "../components/HambergerMenu";
 
 const TopMenu = () => {
+  const menuItems = [
+    { label: "계정설정", onClick: () => alert("메뉴1 클릭됨") },
+    { label: "알림설정", onClick: () => alert("메뉴2 클릭됨") },
+    { label: "관심친구", onClick: () => alert("메뉴3 클릭됨") },
+    { label: "태그설정", onClick: () => alert("메뉴1 클릭됨") },
+    { label: "차단목록", onClick: () => alert("메뉴1 클릭됨") },
+    { label: "구매하기", onClick: () => alert("메뉴1 클릭됨") },
+    { label: "로그아웃", onClick: () => alert("메뉴1 클릭됨") },
+  ];
+
   return (
     <div style={{ display: "flex" }}>
       <div>📃</div>
       <BookStagramTopLogo />
-      <div>🍔</div>
+      <HambergerMenu items={menuItems} />
     </div>
   );
 };
