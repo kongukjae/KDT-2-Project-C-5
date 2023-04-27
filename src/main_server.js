3a83f302cf413968b3819052bf4346fccde22d04
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-27 14:30:31 +0900

    merge용 수정, main_server.js - 테스트용 코드 수정 및 주석 추가.

--- src/main_server.js
+++ src/main_server.js
@@ -13,12 +13,13 @@ const rootPath = path.normalize(currentPath + "\\..")
 const server=http.createServer(function(req,res){
     console.log(req.url);
     if(req.method === "GET"){
-        //index_test용
         if(req.url === "/"){
             res.writeHead(200, {'Content-Type':'text/html'});
-            //파악된 rootPath의 절대경로를 기준으로 다른 파일 경로 지정하기.
-            let htmlData = fs.readFileSync(path.join(rootPath, "../src/index_test.html"), "utf8");
+            // 파악된 rootPath의 절대경로를 기준으로 다른 파일 경로 지정하기.
+            let htmlData = fs.readFileSync(path.join(rootPath, "../src/index.html"), "utf8");
             let userData = "";
+            // BookstargramConnect 사용 방법 예시. signUpQuery에서 정의된 쿼리문 호출 함수를 부르거나, 직접 쿼리문을 작성한다.
+            // 비동기 함수이기에 결과값을 아래 .then()을 사용하는 방식으로 리턴값을 꺼내서 쓰면 된다. 단 Promise로 비동기적으로 실행되기에 write() 및 end()는 이 then 안 콜백에서 사용해야 한다.
             bookstargramConnect(signUpQuery.readAll())
             .then(result=>{
                 userData = userInfo(result);
@@ -30,24 +31,24 @@ const server=http.createServer(function(req,res){
 
             
         }
-        if(req.url === "/signinForm.js"){
-            res.writeHead(200, {'Content-Type':'text/javascript'});
-            res.write(fs.readFileSync(path.join(rootPath,"../dist/signinForm.js"),"utf8"));
-            res.end();
-        }
+        // 하단 <>괄호, 이부분에 번들링된 js파일 경로를 root기준 상대경로로 바꾸시오. (예 : "./dist/bundle.js")
+        // if(req.url === "/<번들링된 파일명>"){
+        //     res.writeHead(200, {'Content-Type':'text/javascript'});
+        //      하단 <>괄호, 이부분에 번들링된 js파일 경로를 root기준 상대경로로 바꾸시오. (예 : "./dist/bundle.js")
+        //      res.write(fs.readFileSync(path.join(rootPath,"<번들링 파일 경로>"),"utf8"));
+        //     res.end();
+        // }
         
     }
     if(req.method === "POST"){
-        if(req.url.startsWith("/login")){
-            
-        }
+        //POST 방식을 사용하는 요청일경우 여기에 작성하시오.
     }
 })
 
-server.listen(4090, err=>{
+server.listen(3000, err=>{
     if(err){
         console.log(`${err}에러가 발생하였습니다.`)
     } else {
-        console.log('포트 4090으로 연결되었습니다.')
+        console.log('포트 3000으로 연결되었습니다.')
     }
 })
