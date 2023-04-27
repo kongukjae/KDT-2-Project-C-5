70295106e2bccda342782871c45b4de939b2f710
jieun <je_eun2735@naver.com>
2023-04-27 14:37:29 +0900

    NavBer.js <Route path=/page3 element={<ReviewPage />} />

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
@@ -6,7 +5,7 @@ import StartPage from "./startScreen";
 import Login from "./login";
 import LoginPage from "./loginPage";
 import SignUpPage from "./signUp";
-import Router from "./router";
+import Router from "../components/router";
 import MainFeed from "./mainFeed";
 import NavBar from "../components/NavBar";
 import "./index.css";
@@ -15,27 +14,11 @@ import BookReviewFeed from "./BookReviewFeed";
 import TopMenu from "./TopMenu";
 
 const App = () => {
-  return <TopMenu />;
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import StartPage from './startScreen';
-import Login from './login';
-import LoginPage from './loginPage';
-import SignUpPage from './signUp';
-import Router from '../components/router';
-import MainFeed from './mainFeed';
-import NavBar from '../components/NavBar';
-import MainFeedReviewForm from '../components/mainFeedReviewForm';
-import ButtonCancle from '../components/ButtonCancle';
-import FollowerFollowing from '../components/FollowerFollowing';
-import './index.css';
-
-
-const App = () => {
-  return <LoginPage/>;
->>>>>>> origin/nowkwon97/issue4
+  return (
+    <div>
+      <TopMenu />
+      <NavBar />
+    </div>
+  );
 };
-
 createRoot(document.getElementById("root")).render(<App />);
