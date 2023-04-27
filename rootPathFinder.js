5bfaaf2bb5a9cc1667bbb8283dd32072a71c8009
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-27 14:46:37 +0900

    merger용 커밋 - 내보낼 파일들 외 삭제완료,

--- rootPathFinder.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import url from "url";
-const currentPath = url.fileURLToPath(import.meta.url);
-const rootPath = path.normalize(currentPath + "\\..")
-
-console.log(path.join(rootPath, "dist/root.json"));
-console.log(rootPath);
-console.log(currentPath);
