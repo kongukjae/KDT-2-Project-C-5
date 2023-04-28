ee251962275f686e4b3dfa722667a2f1842d2b57
jieun <je_eun2735@naver.com>
2023-04-28 17:35:02 +0900

    import Route, Switch

--- components/NoticePage.js
+++ components/NoticePage.js
@@ -1,11 +1,1 @@
-import React from "react";
-
-const NoticePage = () => {
-  return (
-    <div>
-      <div>알림창페이지</div>
-    </div>
-  );
-};
-
-export default NoticePage;
+import { Route, Switch } from "react-router-dom";
