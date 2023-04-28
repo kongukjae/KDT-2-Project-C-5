e08bee7ba7c26c58ad8084149c7a983c6c5f6bf4
leejaekwon <nowkwon97@gmail.com>
2023-04-28 13:08:48 +0900

    border-radius로 인한 테두리 바깥 배경색 처리 방법 탐색

--- components/PopularReview.js
+++ components/PopularReview.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import MainFeedReviewForm from './mainFeedReviewForm';
 
+const formOneStyle = {
+  backgroundColor: '#A7B999',
+  backgroundClip: 'padding-box'
+}
+const formTwoStyle = {
+  backgroundColor: '#B195B1'
+}
+const formThreeStyle = {
+  backgroundColor: '#C86C6C'
+}
 const PopularReview = () => {
   return (
     <div>
-      <MainFeedReviewForm/>
-      <MainFeedReviewForm/>
-      <MainFeedReviewForm/>
+      <div style={formOneStyle}><MainFeedReviewForm/></div>
+      <div style={formTwoStyle}><MainFeedReviewForm/></div>
+      <div style={formThreeStyle}><MainFeedReviewForm/></div>
     </div>
   );
 };
