c5f7a2e071b6c3845c3c1a6f359c5d1452f0d022
jieun <je_eun2735@naver.com>
2023-04-28 17:32:54 +0900

    Link to =/NoticePage

--- components/NotificationTab.js
+++ components/NotificationTab.js
@@ -1,70 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import MainFeedContents from "./MainFeedContents";
-import NoticePage from "./NoticePage";
+import { Link } from "react-router-dom";
 
-const Notification = () => {
+function Notification() {
   return (
     <div>
-      <Link to="/page">📃</Link>
+      <Link to="/NoticePage">
+        <button>📃</button>
+      </Link>
     </div>
   );
-};
+}
 
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/page" element={<NoticePage />} />
-        </Routes>
-        <div>
-          <Notification />
-        </div>
-      </Router>
-    </div>
-  );
-};
-
-export default App;
-// const Notification = () => {
-//   return (
-//     <div>
-//       <Link to="/NoticePage">📃</Link>
-//     </div>
-//   );
-// };
-
-// const App = () =>
-// return(
-//   <div>
-//   <Router>
-//         <Routes>
-//           <Route path="/NoticePage" element={<NoticePage />} />
-
-//         </Routes>
-//   </div>
-// );
-// };
-
-// export default App;
-
-// import React from "react";
-// import NoticePage from "./NoticePage";
-
-// const NotificationTab = () => {
-//   return (
-//     <div>
-//       <h2>Notifications</h2>
-//       <ul>
-//         <li>
-//           <Link to="/noticePge" element={<NoticePage />}>
-//             Notice
-//           </Link>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default NotificationTab;
+export default Notification;
