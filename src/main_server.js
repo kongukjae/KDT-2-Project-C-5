30121647785b5f7c6e8f93883e5d730ff1d1c76f
AmondChocolateShake <dongju230130@gmail.com>
2023-04-29 11:20:04 +0900

    server setting up

---
+++ src/main_server.js
@@ -0,0 +1,18 @@
+import http from "http"
+import fs from "fs"
+
+
+const html=fs.readFileSync("../dist/index.html");
+const js=fs.readFileSync("../dist/main.js")
+const server=http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.end(html);
+    if(res.url=="main.js"){
+        res.end(js);
+    }
+
+})
+
+server.listen(3000,()=>{
+    console.log("서버 열림");
+})
