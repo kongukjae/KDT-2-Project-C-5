1887d21e9d8f1c86f430417ddc1a2c9767475593
AmondChocolateShake <dongju230130@gmail.com>
2023-04-29 11:20:04 +0900

    server setting up

--- src/main_server.js
+++ src/main_server.js
@@ -1,29 +1,16 @@
-import http from "http"
-import fs from "fs"
+import http from "http";
+import fs from "fs";
 
+const html = fs.readFileSync("../dist/index.html");
+const js = fs.readFileSync("../dist/main.js");
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(html);
+  if (res.url == "main.js") {
+    res.end(js);
+  }
+});
 
-// const html=fs.readFileSync("../dist/index.html");
-// const js=fs.readFileSync("../dist/main.js")
-const server=http.createServer(function(req,res){
-
-    console.log(req.url);
-    console.log(req.method);
-    if(req.url === "/" && req.method === "GET") {
-        const html=fs.readFileSync("../dist/index.html");
-        res.writeHead(200,{"Content-Type":"text/html"})
-        res.end(html);     
-    }
-    
-    if(req.url==="/main.js" && req.method==="GET"){
-        console.log("main.js if문 실행");
-        const js=fs.readFileSync("../dist/main.js");
-        res.writeHead(200,{"Content-Type":"text/javascript"});
-        res.end(js);
-    }
-
-})
-
-server.listen(3000,()=>{
-    console.log("서버 열림");
-
-})
+server.listen(3000, () => {
+  console.log("서버 열림");
+});
