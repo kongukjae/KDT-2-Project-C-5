46bcda348f528c6f7897ada9cb8a0a54f7cce84a
AmondChocolateShake <dongju230130@gmail.com>
2023-05-01 11:42:24 +0900

    npm

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (() => {
   "use strict";
 <<<<<<< HEAD
@@ -21,3 +22,6 @@
   e.default.render(t, u);
 >>>>>>> 98f14715 (successed server response, but browser cannot read ESM js file)
 })();
+=======
+(()=>{"use strict";var e=r(require("react-dom"));function r(e){return e&&e.__esModule?e:{default:e}}var t=r(require("react")).default.createElement("div",null,"hello"),u=document.getElementById("root");e.default.render(t,u)})();
+>>>>>>> cc6bd837 (npm)
