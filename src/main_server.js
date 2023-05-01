a34f01b7d3aba2ff0c204e46b8c04c052fb18b8d
AmondChocolateShake <dongju230130@gmail.com>
2023-05-01 09:52:31 +0900

    found a solution to fix webpack and node problem

---
+++ src/main_server.js
@@ -0,0 +1,27 @@
+import http from "http"
+import fs from "fs"
+
+
+// const html=fs.readFileSync("../dist/index.html");
+// const js=fs.readFileSync("../dist/main.js")
+const server=http.createServer(function(req,res){
+
+    console.log(req.url);
+    if(req.url === "/" && req.method === "GET") {
+        const html=fs.readFileSync("../dist/index.html");
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.end(html);     
+    }
+    // res.writeHead(200,{"Content-Type":"text/html"})
+    // res.end(html);
+    if(res.url==="/main.js" && req.method==="GET"){
+        const js=fs.readFileSync("../dist/main.js")
+        res.writeHead(200,{"Content-Type":"text/javascript"})
+        res.end(js);
+    }
+
+})
+
+server.listen(3000,()=>{
+    console.log("서버 열림");
+})
