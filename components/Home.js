0be8b45d5b9c6b96f610706e2275cf321bcd29fb
jieun <je_eun2735@naver.com>
2023-05-01 11:52:20 +0900

    about.js

---
+++ components/Home.js
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        참 사랑이란 거 어렵구나 잡힐 듯 잡힐 듯 하다가도 어느 순간 또 저 멀리
+        있구나 참 바보 같았구나 모든 걸 내줬던 사랑을 받고도 정작 난 그것도
+        몰랐구나 Home You were my home 잠 못 들던 내게 꿈을 꾸게 하던 또 잊혀질
+        일만 남았구나 평생을 안고 가도 모자랄 기억을 나는 또 지워갈 테고 눈 녹듯
+        사라져갈 그날의 모습들 바라보며 나 역시 조금씩 잃어갈 테지 Home You were
+        my home 잠 못 들던 내게 꿈을 꾸게 하던 You were my home away from home
+        움츠린 내 맘을 당당하게 하던 You were my home You are my home You’re
+        still my home 이 세상 어딘가에 있단 것만으로 You give me hope to carry
+        on 기억만으로도 내 심장을 뛰게 해 You are still my home
+      </p>
+    </div>
+  );
+};
+
+export default Home;
