f73359e6f8552b036642762ef9da8c47b4ce949c
AmondChocolateShake <dongju230130@gmail.com>
2023-05-01 10:39:45 +0900

    successed server response, but browser cannot read ESM js file

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,10 @@
-(()=>{"use strict";var e=require("react-dom/client"),r=u(require("react")),t=u(require("./loginPage"));function u(e){return e&&e.__esModule?e:{default:e}}(0,e.render)(r.default.createElement(t.default,null),document.getElementById("root"))})();
+(() => {
+  "use strict";
+  var e = r(require("react-dom"));
+  function r(e) {
+    return e && e.__esModule ? e : { default: e };
+  }
+  var t = r(require("react")).default.createElement("div", null, "hello"),
+    u = document.getElementById("root");
+  e.default.render(t, u);
+})();
