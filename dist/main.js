32b4cb079ab819383bda6b4d4eb2f8348e566a4e
AmondChocolateShake <dongju230130@gmail.com>
2023-05-01 11:42:24 +0900

    npm

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,1 @@
-(() => {
-  "use strict";
-  var e = r(require("react-dom"));
-  function r(e) {
-    return e && e.__esModule ? e : { default: e };
-  }
-  var t = r(require("react")).default.createElement("div", null, "hello"),
-    u = document.getElementById("root");
-  e.default.render(t, u);
-})();
+(()=>{"use strict";var e=r(require("react-dom"));function r(e){return e&&e.__esModule?e:{default:e}}var t=r(require("react")).default.createElement("div",null,"hello"),u=document.getElementById("root");e.default.render(t,u)})();
