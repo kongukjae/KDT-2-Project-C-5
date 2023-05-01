02870b84c5877a2c9b2d82f5404ba285a12d4e17
AmondChocolateShake <dongju230130@gmail.com>
2023-05-01 10:33:36 +0900

    main.js 요청 응답 안되서 설정 바꿈

--- src/main_server.js
+++ src/main_server.js
@@ -16,6 +16,23 @@ const server = http.createServer(function (req, res) {
   }
 });
 
-server.listen(3000, () => {
-  console.log("서버 열림");
-});
+    console.log(req.url);
+    if(req.url === "/" && req.method === "GET") {
+        const html=fs.readFileSync("../dist/index.html");
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.end(html);     
+    }
+    // res.writeHead(200,{"Content-Type":"text/html"})
+    // res.end(html);
+    if(res.url==="/main.js" && req.method==="GET"){
+        console.log("main.js if문 실행");
+        const js=fs.readFileSync("../dist/main.js");
+        res.writeHead(200,{"Content-Type":"text/javascript"});
+        res.end(js);
+    }
+
+})
+
+server.listen(3000,()=>{
+    console.log("서버 열림");
+})
