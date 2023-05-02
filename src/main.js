ea2640f5d7ecab66afb3c71d99f249869e15bbea
jieun <je_eun2735@naver.com>
2023-05-02 14:45:57 +0900

    main작업환경 통합 완료

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import StartPage from "./startScreen";
-import Login from "./login";
-import LoginPage from "./loginPage";
-import SignUpPage from "./signUp";
-import Router from "../components/router";
-import MainFeed from "./mainFeed";
-import NavBar from "../components/NavBar";
+// import StartPage from "./startScreen";
+// import Login from "./login";
+// import LoginPage from "./loginPage";
+// import SignUpPage from "./signUp";
+// import Router from "../components/router";
+// import MainFeed from "./mainFeed";
+// import NavBar from "../components/NavBar";
 import "./index.css";
-import ReviewPage from "./ReviewPage";
-import BookReviewFeed from "./BookReviewFeed";
+// import ReviewPage from "./ReviewPage";
+// import BookReviewFeed from "./BookReviewFeed";
 import TopMenu from "./TopMenu";
-import NotificationTab from "../components/NotificationTab";
+// import NotificationTab from "../components/NotificationTab";
 
 const App = () => {
   return (
