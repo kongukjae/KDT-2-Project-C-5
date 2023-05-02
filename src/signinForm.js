1a44fec552e8bfe1d0d34108d82cb700855f88fb
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-02 16:47:22 +0900

    로그인 페이지 구현 및 서버 통신 시험 - 회원가입 form 추가

--- src/signinForm.js
+++ src/signinForm.js
@@ -10,6 +10,25 @@ function signInFormData(){
       <input type="password" name="password" />
     </label>
     <button type="submit">로그인</button>
+  </form>
+  <form method="POST" action="/signin">
+    <label>
+      ID:
+      <input type="text" name="id" />
+    </label>
+    <label>
+      Password:
+      <input type="password" name="password" />
+    </label>
+    <label>
+      Email:
+      <input type="text" name="uEmail" />
+    </label>
+    <label>
+      유저 닉네임:
+      <input type="text" name="uName" />
+    </label>
+    <button type="submit">회원가입</button>
   </form>`
 }
 function signInForm(){
