aa567cd48dbf6009c0aa132a88595b0a325a0b97
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-02 15:00:33 +0900

    formFetch의 handleSubmit을 loginPage에 적용.

--- src/loginPage.js
+++ src/loginPage.js
@@ -9,11 +9,22 @@ import ButtonSignUp from '../components/ButtonSignUp';
 import ForgotAccount from '../components/ForgotAccount';
 
 const loginPage = () => {
+  function handleSubmit(event) {
+    //새창을 여는 기본 설정을 방지하는 구문.
+    event.preventDefault();
+
+    //받은 이벤트의 타겟, submit된 데이터(id, password)를 form으로 저장하는 구문
+    const form = event.target;
+    //해당 타겟을 FormData 형식으로 변환, 저장하는 구문.
+    const formData = new FormData(form);
+    //fetch로 서버에 데이터를 보내는 구문.
+    fetch("/login", {method: form.method, body: formData})
+}
   return (
     <div>
       <BookStagramTopLogo/>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <InputId/>
           <p><b>password</b></p>
           <InputPwd/>
