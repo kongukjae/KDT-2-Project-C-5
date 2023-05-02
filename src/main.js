438fa23c054cd1c4547bb9a02b53f9db2a06bc6b
jieun <je_eun2735@naver.com>
2023-05-02 14:41:03 +0900

    main.js충돌

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import StartPage from './startScreen';
-import LoginPage from './loginPage';
-// import SignUpPage from './signUp';
-// import Router from '../components/router';
-// import MainFeed from './mainFeed';
-// import NavBar from '../components/NavBar';
-// import MainFeedReviewForm from '../components/mainFeedReviewForm';
-// import ButtonCancle from '../components/ButtonCancle';
-// import FollowerFollowing from '../components/FollowerFollowing';
-import './index.css';
-
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
@@ -29,7 +13,6 @@ import ReviewPage from "./ReviewPage";
 import BookReviewFeed from "./BookReviewFeed";
 import TopMenu from "./TopMenu";
 import NotificationTab from "../components/NotificationTab";
->>>>>>> origin/issue18-2
 
 const App = () => {
   return (
