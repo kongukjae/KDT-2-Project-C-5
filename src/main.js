af20394caf3162957547fc3b568d19f086fed0b0
AmondChocolateShake <dongju230130@gmail.com>
2023-05-03 17:44:55 +0900

    3sec

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import React from "react";
-
 import { createRoot } from "react-dom/client";
 
 import NavBar from "../components/NavBar";
+import Logo from "./startScreen"
+
 import "./index.css";
+
+
+
 
 
 const App = () => {
   return (
     
-    <NavBar></NavBar>
+    <Logo></Logo>    
 
   );
 };
--- src/startScreen.js
+++ src/startScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const test = () => {
+const logo = () => {
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      history.push('/signUp');
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return <div>
     <div id='start-logo'>BOOK STAGRAM</div>
     </div>
 }
 
-export default test;
+export default logo;
