daa08af062a3565e455871c1cf947bd85e9bed2c
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-03 17:14:50 +0900

    객체 전달 테스트를 위해 main을 login으로 변환

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-
-import NavBar from "./NavBar";
-import Logo from "./startScreen"
-import LoginPage from "./loginPage";
-import SignUp from "./signUp";
-import AccountSetting from "./AccountSetting";
-import BlockedUser from "./BlockedUser";
-import FollowerFollowing from "./FollowerFollowing";
-import ReviewPage from "./ReviewPage";
-import MainFeed from "./mainFeed";
-
-
+import LoginPage from "./loginPage"
+//import NavBar from "../components/NavBar";
 import "./index.css";
 
 
@@ -22,22 +10,7 @@ import "./index.css";
 
 const App = () => {
   return (
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<Logo/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signUp" element={<SignUp/>} />
-        <Route path="/accountSetting" element={<AccountSetting/>} />
-        <Route path="/lockedUser" element={<BlockedUser/>} />
-        <Route path="/mainFeed" element={<NavBar/>} />
-        <Route path="/followerSetting" element={<FollowerFollowing/>}/>
-        <Route path="/review" element={<ReviewPage/>}/>
-
-      </Routes>
-    
-    </Router>   
-
+    <LoginPage />
   );
 };
 createRoot(document.getElementById("root")).render(<App />);
