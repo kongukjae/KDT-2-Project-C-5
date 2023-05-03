cef712eefe521719d21906f52ec61e90f5a62099
leejaekwon <nowkwon97@gmail.com>
2023-05-03 11:05:10 +0900

    server에서 loginpage 동작 반응 테스트

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import React from "react";
-
 import { createRoot } from "react-dom/client";
-
 import NavBar from "../components/NavBar";
+import LoginPage from "./loginPage";
 import "./index.css";
 
 
 const App = () => {
   return (
     <div>
-      <NavBar/>
+      <LoginPage/>
     </div>
   );
 };
