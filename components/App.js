bda249b4113acfd561705b7b54acf62ed5f612e7
jieun <je_eun2735@naver.com>
2023-05-03 14:20:03 +0900

    issue18-2에서 새롭게 다시 태어남

--- components/App.js
+++ components/App.js
@@ -1,13 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+
   );
 };
 
