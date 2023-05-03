c94dd7d3d9500018ec642753ad8421fbfd86b4b9
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-03 11:02:58 +0900

    webpack 번들링시 에러나는 문제 해결- main.js에 없는 import 파일 제거

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import StartPage from "./startScreen";
-import Login from "./login";
-import LoginPage from "./loginPage";
-import SignUpPage from "./signUp";
-import Router from "../components/router";
-import MainFeed from "./mainFeed";
 import NavBar from "../components/NavBar";
 import "./index.css";
-import ReviewPage from "./ReviewPage";
-import BookReviewFeed from "./BookReviewFeed";
-import FollowerFollowing from './FollowerFollowing';
-import AlarmSetting from "../components/AlarmSetting";
-import BlockedUser from "./BlockedUser";
-import AccountSetting from "./AccountSetting";
-import TopMenu from "./TopMenu";
+
 
 const App = () => {
   return (
