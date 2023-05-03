ddd46a690d3ad9da2102d105ab18c57a9a1c6215
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-03 18:08:29 +0900

    회원가입 페이지 기능 추가 Fixes #45 - main.js 연결 페이지 변경

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 import { createRoot } from "react-dom/client";
-import LoginPage from "./loginPage"
+import SignUp from "./signUp"
+//import LoginPage from "./loginPage"
 //import NavBar from "../components/NavBar";
 import "./index.css";
 
 
 const App = () => {
   return (
-    <LoginPage />
+    <SignUp />
   );
 };
 createRoot(document.getElementById("root")).render(<App />);
