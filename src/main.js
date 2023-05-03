80345bcf49691d05c408e4817a43775a9675165e
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-03 17:14:50 +0900

    객체 전달 테스트를 위해 main을 login으로 변환

--- src/main.js
+++ src/main.js
@@ -16,17 +16,15 @@ import './index.css';
 import React from "react";
 
 import { createRoot } from "react-dom/client";
-
-import NavBar from "../components/NavBar";
+import LoginPage from "./loginPage"
+//import NavBar from "../components/NavBar";
 import "./index.css";
 >>>>>>> 0bbcf6781f68dc5a7e1be06cf83286d30776fc68
 
 
 const App = () => {
   return (
-    <div>
-      <NavBar/>
-    </div>
+    <LoginPage />
   );
 };
 createRoot(document.getElementById("root")).render(<App />);
