6726938aa775aa7b0ec8ab3dbcd0c727e2b13abe
jungjieun <je_eun2735@naver.com>
2023-05-04 13:03:44 +0900

    issue25에서 작성하던 파일 추가

---
+++ mariaDB.js
@@ -0,0 +1,34 @@
+import mariaDB from "mariadb";
+import http, { createServer } from "http";
+
+async function main() {
+  let conn;
+  try {
+    conn = await mariaDB.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "xxia1215@@",
+      database: "bookstagram",
+    });
+    const data = await conn.query("SELECT * FROM userinfo");
+    // console.log(rows);
+    return data;
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) {
+      conn.end();
+    }
+  }
+}
+
+const server = createServer(async function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  const data = await main(); // main() 함수에서 data 받기
+  res.write(JSON.stringify(data)); // JSON 형식으로 data 작성
+  //[{"id":8,"user-id":"jieun","user-pwd":"1234","user-email":"jieun@email.com","user-name":"jiny"}]
+
+  res.end();
+});
+
+server.listen(3000);
