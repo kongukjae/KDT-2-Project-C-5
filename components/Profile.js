23e1ac26029cbdd55299039246257919635b4524
AmondChocolateShake <dongju230130@gmail.com>
2023-05-05 21:32:04 +0900

    introduce box done

---
+++ components/Profile.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+
+//마이페이지에 사용되는 프로필 컴포넌트
+const profile=()=>{
+  
+
+  return(
+    <div>
+
+
+    </div>
+  )
+
+}
+
+export default profile;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
+import Introduce from "../components/Introduce";
 import NavBar from "./NavBar";
 import Logo from "./startScreen"
 import LoginPage from "./loginPage";
@@ -27,23 +28,23 @@ const Page5 = () => <h1>내 정보 피드</h1>;
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Logo/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signUp" element={<SignUp/>} />
-        <Route path="/accountSetting" element={<AccountSetting/>} />
-        <Route path="/lockedUser" element={<BlockedUser/>} />
-        <Route path="/mainFeed" element={<MainFeed/>} />
-        <Route path="/followerSetting" element={<FollowerFollowing/>}/>
-        <Route path="/review" element={<ReviewPage/>}/>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Logo/>} />
+    //     <Route path="/login" element={<LoginPage/>} />
+    //     <Route path="/signUp" element={<SignUp/>} />
+    //     <Route path="/accountSetting" element={<AccountSetting/>} />
+    //     <Route path="/lockedUser" element={<BlockedUser/>} />
+    //     <Route path="/mainFeed" element={<MainFeed/>} />
+    //     <Route path="/followerSetting" element={<FollowerFollowing/>}/>
+    //     <Route path="/review" element={<ReviewPage/>}/>
         
         
         
-      </Routes>
+    //   </Routes>
     
-    </Router>   
-
+    // </Router>   
+    <Introduce></Introduce>
   );
 };
 createRoot(document.getElementById("root")).render(<App />);
