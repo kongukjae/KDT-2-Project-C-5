5caa48843fd6bdf24fad68c188dbd353beecc0be
jungjieun <je_eun2735@naver.com>
2023-05-07 12:49:21 +0900

    post save...?

--- src/ReviewPage.js
+++ src/ReviewPage.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import PostInput from "../components/PostInput";
 import Search from "../components/Search";
+import Post from "../components/Post";
 
 function ReviewPage() {
+  const [posts, setPosts] = useState([]);
+
+  const handleSave = (post, isEditing) => {
+    if (isEditing) {
+      const updatedPosts = posts.map((p) => {
+        if (p.id === post.id) {
+          return post;
+        }
+        return p;
+      });
+      setPosts(updatedPosts);
+    } else {
+      setPosts([...posts, post]);
+    }
+  };
+
   return (
     <div
       className="container"
@@ -15,7 +32,7 @@ function ReviewPage() {
       }}
     >
       <Search />
-      <PostInput />
+      <PostInput onSave={handleSave} />
     </div>
   );
 }
