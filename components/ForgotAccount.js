8a4c508f684655cea8b85fc4d9d51e0a83c547da
AmondChocolateShake <dongju230130@gmail.com>
2023-05-08 17:54:55 +0900

    login page styling done

--- components/ForgotAccount.js
+++ components/ForgotAccount.js
@@ -1,9 +1,17 @@
 // 로그인 시 사용되는 컴포넌트이다.
 import React from 'react';
 
+const forgotSt={
+  display:"flex",
+  flexDirection:"column",
+  alignItems:"center",
+  color:"D9D9D9",
+  fontSize:"7px"
+}
+
 const ForgotAccount = () => {
   return (
-    <div>
+    <div style={forgotSt}>
       <a href=''>forgot account?</a>
     </div>
   );
