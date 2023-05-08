1897a5c3df2430c15ab1e8018f88bd7691966fce
jieun <je_eun2735@naver.com>
2023-05-08 17:39:04 +0900

    PopularReview.js profilePic

--- src/bookReviewFeed.js
+++ src/bookReviewFeed.js
@@ -4,6 +4,8 @@ import BookDescription from "../components/BookDescription-JE";
 import FeedTopBar from "../components/FeedTopBar";
 import UploadTime from "../components/UploadTime-JE";
 
+//메인피드에서 책사진 클릭시 나타나는 긴글리뷰 페이지
+//정지은
 const BookReviewFeed = () => {
   return (
     <div style={{ backgroundColor: "#DCDCDC" }}>
