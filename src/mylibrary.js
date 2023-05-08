4f8c69cd6de7bffcb78d34db020af18514ba7657
wjkk21 <verstand821@gmail.com>
2023-05-08 18:05:29 +0900

    mylibrary.js 생성

---
+++ src/mylibrary.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from './NavBar';
+
+/* 내 페이지 안의 내 서재 
+수정 :임지성
+ */
+const mylibrary = () => {
+return 
+  <div>
+
+
+      <footer>
+        <NavBar/>
+      </footer>
+  </div>
+}
+
+export default mylibrary;
