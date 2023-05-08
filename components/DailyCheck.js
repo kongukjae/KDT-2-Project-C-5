66f28486486b85c32fe1089dcbcdb82749773c83
leejaekwon <nowkwon97@gmail.com>
2023-05-08 09:28:03 +0900

    timer, dailycheck add

---
+++ components/DailyCheck.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DailyCheck = () => {
+  return (
+    <div>
+      출석체크
+    </div>
+  );
+};
+
+export default DailyCheck;
---
+++ components/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Timer = () => {
+  return (
+    <div>
+      타이머
+    </div>
+  );
+};
+
+export default Timer;
