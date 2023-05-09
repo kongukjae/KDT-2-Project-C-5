3874aa413d5a335cedd6d253a5573584e8a62f92
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-09 14:30:43 +0900

    TagCheckbox의 value를 이름으로 설정

--- components/TagCheckbox.js
+++ components/TagCheckbox.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function TagCheckbox(props) {
   return(
     <label>
-      <input type="checkbox"name={props.name} /> {props.name}
+      <input type="checkbox"name="tag" value={props.name}/> {props.name}
     </label>
   )
 }
