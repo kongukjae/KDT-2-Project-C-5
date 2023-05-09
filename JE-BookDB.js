2a1fbed76653c3da71473007561e5cdaf6bf42c6
jungjieun <je_eun2735@naver.com>
2023-05-09 14:55:47 +0900

    thumbnailUrl

--- JE-BookDB.js
+++ JE-BookDB.js
@@ -74,19 +74,35 @@ const url = "https://jsonplaceholder.typicode.com/todos";
 //   });
 // };
 // API에 GET 요청 보내기
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    // 파싱한 데이터를 콘솔에 출력
-    data.forEach((item) => {
-      console.log(item);
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // 파싱한 데이터를 콘솔에 출력
+//     data.forEach((item) => {
+//       console.log(item);
+//     });
+//   });
+// Google Books API에서 책 정보와 표지 이미지를 가져오는 함수
+function getBookInfo(title) {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${title}`; // API URL
+  https.get(url, (response) => {
+    let data = "";
+    // API 응답 데이터 수신 시
+    response.on("data", (chunk) => {
+      data += chunk;
+      console.log(data);
+    });
+    response.on("end", () => {
+      const bookInfo = JSON.parse(data).items[0].volumeInfo; // 첫 번째 검색 결과에서 책 정보 가져오기
+      // console.log(bookInfo);
+      const thumbnailUrl = bookInfo.imageLinks.thumbnail; // 책 정보에서 표지 이미지 URL 가져오기
+      console.log(thumbnailUrl);
     });
   });
-// Google Books API에서 책 정보와 표지 이미지를 가져오는 함수
-  function getBookInfo(title) {
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${title}`; // API URL
-    https.get(url, (response) => {
-      let data = "";
+}
+//data 안의 title 입력
+getBookInfo("The Hitchhiker's Guide to the Galaxy");
+getBookInfo("Sustainable Development and Planning VIII");
 
 const connection = mysql.createConnection({
   host: "127.0.0.1",
