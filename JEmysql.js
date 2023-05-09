607db3431baf354a412d043ecb627e5b65f42ccc
jungjieun <je_eun2735@naver.com>
2023-05-09 11:41:15 +0900

    books 테이블 생성

--- JEmysql.js
+++ JEmysql.js
@@ -3,21 +3,32 @@ import mysql from "mysql";
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "xxia1215@@",
+  password: "Xxia1215@@",
   database: "bookstagram",
 });
 
+connection.connect();
 // 데이터베이스 연결
-connection.connect((err) => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("연결 성공");
-});
+const books = "SELECT * FROM books";
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("연결 성공");
+// });
 
 //데이터 조회
-connection.query("SELECT * FROM userinfo", (error, results, fields) => {
+// connection.query("SELECT * FROM userinfo", (error, results, fields) => {
+//   if (error) {
+//     console.error("데이터베이스 조회 실패: " + error.stack);
+//     return;
+//   }
+//   console.log(results);
+// });
+
+connection.query(books, (error, results, fields) => {
   if (error) {
     console.error("데이터베이스 조회 실패: " + error.stack);
     return;
