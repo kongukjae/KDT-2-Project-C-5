77caa06584d71dcf171ef44f2b0d5a297308c74f
jungjieun <je_eun2735@naver.com>
2023-05-09 12:43:51 +0900

    N오타수정

--- JE-BookDB.js
+++ JE-BookDB.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import https from "https";
 const url = "https://www.googleapis.com/books/v1/volumes";
 
@@ -13,6 +14,15 @@ import mysql from "mysql";
 const connection = mysql.createConnection({
   host: "localhost",
 >>>>>>> bc3f73c8 (books 테이블 생성)
+=======
+import https from "https";
+const url = "https://www.googleapis.com/books/v1/volumes";
+
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+>>>>>>> f87864c7 (N오타수정)
   user: "root",
   password: "Xxia1215@@",
   database: "bookstagram",
@@ -22,9 +32,13 @@ connection.connect();
 // 데이터베이스 연결
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const userinfo = "SELECT * FROM userinfo";
 =======
 >>>>>>> bc3f73c8 (books 테이블 생성)
+=======
+const userinfo = "SELECT * FROM userinfo";
+>>>>>>> f87864c7 (N오타수정)
 const books = "SELECT * FROM books";
 
 connection.query(books, (error, results, fields) => {
@@ -34,6 +48,7 @@ connection.query(books, (error, results, fields) => {
   }
   console.log(results);
 <<<<<<< HEAD
+<<<<<<< HEAD
   connection.end();
 });
 =======
@@ -41,3 +56,7 @@ connection.query(books, (error, results, fields) => {
 
 // const url = `https://www.googleapis.com/books/v1/volumes?q=${title}`; // API URL
 >>>>>>> bc3f73c8 (books 테이블 생성)
+=======
+  connection.end();
+});
+>>>>>>> f87864c7 (N오타수정)
