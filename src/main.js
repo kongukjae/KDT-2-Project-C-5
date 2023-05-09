9f9c996b4b0d805e98813c8bfc7e78ecd2c95bfb
jungjieun <je_eun2735@naver.com>
2023-05-09 18:24:53 +0900

    API 이미지url에서 이미지 데이터 추출 후 저장 Fixes #79 + public 경로 변경

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ import ChallengePage from "./challengePage";
 import "./index.css";
 import NotificationList from "../components/NotificationList";
 import FavorTag from "./favortag";
+import BookReviewFeed from "./bookReviewFeed";
 
 // 테스트를 위한 임시 코드
 const Page1 = () => <h1>실시간, 인기 리뷰 피드</h1>;
@@ -28,22 +29,23 @@ const Page5 = () => <h1>내 정보 피드</h1>;
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Logo />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/accountSetting" element={<AccountSetting />} />
-        <Route path="/lockedUser" element={<BlockedUser />} />
-        <Route path="/mainFeed" element={<MainFeed />} />
-        <Route path="/challenge" element={<ChallengePage />} />
-        <Route path="/followerSetting" element={<FollowerFollowing />} />
-        <Route path="/review" element={<ReviewPage />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/searchPage" element={<SearchPage />} />
-        <Route path="/NotificationList" element={<NotificationList />} />
-      </Routes>
-    </Router>
+    <BookReviewFeed />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Logo />} />
+    //     <Route path="/login" element={<LoginPage />} />
+    //     <Route path="/signUp" element={<SignUp />} />
+    //     <Route path="/accountSetting" element={<AccountSetting />} />
+    //     <Route path="/lockedUser" element={<BlockedUser />} />
+    //     <Route path="/mainFeed" element={<MainFeed />} />
+    //     <Route path="/challenge" element={<ChallengePage />} />
+    //     <Route path="/followerSetting" element={<FollowerFollowing />} />
+    //     <Route path="/review" element={<ReviewPage />} />
+    //     <Route path="/mypage" element={<Mypage />} />
+    //     <Route path="/searchPage" element={<SearchPage />} />
+    //     <Route path="/NotificationList" element={<NotificationList />} />
+    //   </Routes>
+    // </Router>
   );
 };
 createRoot(document.getElementById("root")).render(<App />);
