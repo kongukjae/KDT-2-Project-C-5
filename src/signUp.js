f3d7583f65dea42ba388a30d18cf91eab99aa09d
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-09 14:50:48 +0900

    signUp을 form으로 변경 후 submit하여 이를 정제하는 구문 추가

--- src/signUp.js
+++ src/signUp.js
@@ -1,6 +1,7 @@
 // 회원 가입 컴포넌트이다.
 import React from 'react';
-import {BrowserRouter as Routes,Route, Link} from 'react-router-dom'
+import { BrowserRouter as Routes, Route, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import BookStagramTopLogo from '../components/BookStagramTopLogo';
 import InputId from '../components/InputId';
 import InputPwd from '../components/InputPwd';
@@ -13,21 +14,36 @@ import './index.css';
 
 
 const signUp = () => {
+  //네비게이트 함수 사용
+  const navigate = useNavigate();
+  //submit이 눌렸을 경우 실행하는 함수
+  function submitSignUpForm(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const signUpForm = {};
+    for (let [name, value] of formData.entries()) {
+      signUpForm[name] = value;
+    }
+  }
+  navigate("/favortag", signUpForm)
+
+
   return (
-    
-    <div>
-      <BookStagramTopLogo/>
-      <InputProfilePic/>
-      <InputId/>
-      <InputEmail/>
-      <InputUserName/>
+
+    <form type="method" onSubmit={submitSignUpForm}>
+      <BookStagramTopLogo />
+      <InputProfilePic />
+      <InputId />
+      <InputEmail />
+      <InputUserName />
       {/* <InputPwd/> */}
-      <InputPwdCheck/>
+      <InputPwdCheck />
       <Link to="/favortag">
-      <ButtonSubmit/>
+        <ButtonSubmit />
       </Link>
-    </div>
+    </form>
   );
 };
+
 
 export default signUp;
