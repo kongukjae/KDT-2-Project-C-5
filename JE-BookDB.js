1b43f2a8f2af05b1dcdf349479d53e4516ee92b5
jungjieun <je_eun2735@naver.com>
2023-05-09 13:04:53 +0900

    url 변경

--- JE-BookDB.js
+++ JE-BookDB.js
@@ -57,22 +57,31 @@ getBookInfo("Sustainable Development and Planning VIII");
 
 import https from "https";
 import mysql from "mysql";
-const url = "https://www.googleapis.com/books/v1/volumes";
-// JSON 데이터를 반환하는 외부 API URL
-const xhr = new XMLHttpRequest();
+const url = "https://jsonplaceholder.typicode.com/todos";
+// // JSON 데이터를 반환하는 외부 API URL
+// const xhr = new XMLHttpRequest();
+// // API에 GET 요청 보내기
+// xhr.open("GET", url);
+// xhr.send();
+
+// xhr.onload = function () {
+//   // 응답으로 받은 JSON 데이터 파싱
+//   const data = JSON.parse(xhr.responseText);
+
+//   // 파싱한 데이터를 콘솔에 출력
+//   data.forEach((item) => {
+//     console.log(item);
+//   });
+// };
 // API에 GET 요청 보내기
-xhr.open("GET", url);
-xhr.send();
-
-xhr.onload = function () {
-  // 응답으로 받은 JSON 데이터 파싱
-  const data = JSON.parse(xhr.responseText);
-
-  // 파싱한 데이터를 콘솔에 출력
-  data.forEach((item) => {
-    console.log(item);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // 파싱한 데이터를 콘솔에 출력
+    data.forEach((item) => {
+      console.log(item);
+    });
   });
-};
 
 const connection = mysql.createConnection({
   host: "127.0.0.1",
