626a7358fd72d54ce7c5dbbf3208450459eb5547
AmondChocolateShake <dongju230130@gmail.com>
2023-05-09 11:35:21 +0900

    trying to add data to arrary

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -25,11 +25,12 @@ const container={
 //태그 및 도서 검색 페이지 담당 컴폰넌트
 //김동주
 const searchPage=()=>{
-  const[tag,setTag]=useState([])
+  const[tagList,setTagList]=useState([])
   
-
+  //리액트 전역관리
   function getTag(t){
-    console.log(t)
+    console.log(t);
+    setTagList();
   }
 
 
