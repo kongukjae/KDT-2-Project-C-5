9a38e908d80b0812ce848101540375dea5405d04
jungjieun <je_eun2735@naver.com>
2023-05-10 11:34:30 +0900

    from>input,button

--- ReviewPage2.js
+++ ReviewPage2.js
@@ -3,16 +3,50 @@ import PostInput from "./components/PostInput";
 // import Search from "../components/Search";
 import NavBar from "./src/NavBar";
 import TopMenu from "./src/TopMenu";
+
 const styles = {
-  fontSize: "24px",
-  marginTop: "10px",
-  width: "150px",
-  height: "150px",
-  backgroundColor: "#DCDCDC",
-  borderRadius: "4px",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
+  header: {
+    fontSize: "24px",
+    marginTop: "10px",
+    width: "150px",
+    height: "150px",
+    backgroundColor: "#DCDCDC",
+    borderRadius: "4px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  wrapper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "20px",
+  },
+  button: {
+    padding: "10px 20px",
+    backgroundColor: "#696969",
+    color: "#fff",
+    fontSize: "16px",
+    border: "none",
+    borderRadius: "4px",
+    marginLeft: "10px",
+    cursor: "pointer",
+  },
+  input: {
+    width: "200px",
+    height: "70x",
+    padding: "5px 10px",
+    fontSize: "16px",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginTop: "10px",
+  },
 };
 
 function ReviewPage2() {
@@ -20,8 +54,8 @@ function ReviewPage2() {
     <div>
       <TopMenu />
       <form>
-        <div style={styles}></div>
-        <input />
+        <div style={styles.header}></div>
+        <input type="text" placeholder="search for book" style={styles.input} />
         <button type="submit" style={styles.button}>
           검색
         </button>
