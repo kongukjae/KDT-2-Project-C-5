d59c417c22e7f758cfa8cd8ed78926c5489caba3
DGChoi3904 <dgchoi3904@gmail.com>
2023-05-23 10:22:56 +0900

    main_server 측 리뷰 폼 DB에 저장 및 저장된 행 호출 반환

--- src/main_server.js
+++ src/main_server.js
@@ -1,6 +1,7 @@
 import http from "http"
 import fs from "fs"
 import sendQuery from "./mariadb.js"
+import { send } from "process";
 
 
 // const html=fs.readFileSync("../dist/index.html");
@@ -23,61 +24,93 @@ const server = http.createServer(function (req, res) {
             res.end(js);
         }
     }
-    if (req.method === "POST"){
-        if (req.url === "/login"){
-            req.on("data", chunk=>{
+    if (req.method === "POST") {
+        if (req.url === "/login") {
+            req.on("data", chunk => {
                 // post로 받은 데이터(JSON)을 parse하여 객체로 변환
                 const data = JSON.parse(chunk);
                 // 쿼리문을 보낼 때, 열은 백틱(``)으로 하고, 비교할 데이터는 작은따옴표('')를 써서 문자열이라 표기했다. 만일 전부 백틱으로 할 경우, data.id를 열중 하나로 인식하는 문제가 있었다.
                 sendQuery(`SELECT * FROM \`bookstargram\`.\`userinfo\` WHERE \`user-id\`= '${data.id}'`)
-                .then(result=>{
-                    if(result){
-                        //query문의 반환값이 있을 경우, 하나의 인덱스마다 객체를 받는 배열이 result에 할당된다. 위의 Query는 1행만 반환하기에 인덱스를0,키값을 DB테이블에서 정의된 값으로 했다.
-                        if(result[0][`user-pwd`] === data.pwd){
-                            console.log("Login Success!")
-                            return true;
-                        } else {
-                            console.log("Login Failed!")
-                            return false;
+                    .then(result => {
+                        if (result) {
+                            //query문의 반환값이 있을 경우, 하나의 인덱스마다 객체를 받는 배열이 result에 할당된다. 위의 Query는 1행만 반환하기에 인덱스를0,키값을 DB테이블에서 정의된 값으로 했다.
+                            if (result[0][`user-pwd`] === data.pwd) {
+                                console.log("Login Success!")
+                                return true;
+                            } else {
+                                console.log("Login Failed!")
+                                return false;
+                            }
                         }
-                    }
-                })
-                //로그인이 성공했을 때만 메인피드 페이지로 이동함.
-                .then(result =>{
-                    console.log(result);
-                    if(result){
-                        const sender = {result : result}
-                        console.log(sender);
-                        res.writeHead(200,{"Content-Type":"application/json"});
-                        res.end(JSON.stringify(sender));
-                    }
-                })
+                    })
+                    //로그인이 성공했을 때만 메인피드 페이지로 이동함.
+                    .then(result => {
+                        console.log(result);
+                        if (result) {
+                            const sender = { result: result }
+                            console.log(sender);
+                            res.writeHead(200, { "Content-Type": "application/json" });
+                            res.end(JSON.stringify(sender));
+                        }
+                    })
             })
 
-            if(req.url === "/reivew"){
-                req.on("data", chunk=>{
+            if (req.url === "/reivew") {
+                req.on("data", chunk => {
                     // post로 받은 데이터(JSON)을 parse하여 객체로 변환
-                    const data = JSON.parse(chunk);
-            })
+                    // const sample = {
+                    //     "userid": "dgchoi3904",
+                    //     "booktitle": "개리포터와 개법사의 뼈",
+                    //     "bookcover": null,
+                    //     "bookauthor": "롤링스파이더",
+                    //     "bookpublisher": "문학쇼츠",
+                    //     "bookrelease": "20230518",
+                    //     "isbn": "9781234567890",
+                    //     "summery": "짧은 리뷰 내용",
+                    //     "body": "리뷰 내용",
+                    //     "tag": "#없어!",
+                    // }
+                    const bookreviewForm = JSON.parse(chunk);
+                    // 책리뷰 등록 쿼리문
+                    sendQuery(`INSERT INTO bookreview (userid, username, userpic, booktitle, bookcover, bookauthor, bookpublisher, isbn, summery, body, tag)
+                    SELECT u.\`user-id\`, u.\`user-name\`, u.\`user-pic\`, ${data.booktitle}, ${data.bookcover}, ${data.bookauthor},${data.bookpublisher}, ${data.isbn},${data.summery},  ${data.body}, ${data.tag}
+                    FROM userinfo u
+                    WHERE u.\`user - id\` = ${data.userid}`)
+                    .then(()=>{
+                        // 등록한 책리뷰의 ID, index 요청 쿼리문(바로 전 단계에 추가된 하나의 행의 id값만을 가져오는 구문이니 주의할 것.(2행 이상일 결우 그전 행의 값만 가져올 수 있다.))
+                        const yourReviewIndex = sendQuery(`SELECT LAST_INSERT_ID()`)
+                        return yourReviewIndex;
+                    })
+                    .then(result=>{
+                        const body = {};
+                        // 받은 index 값을 토대로 작성한 행에 조회
+                        body.yourReview = sendQuery(`SELECT * FROM \`bookstargram\`.\`bookreview\` WHERE \`index\` = ${result}`)
+                        res.writeHead(200,{"Content-Type":"application/json"});
+                        res.write(JSON.stringify(body));
+                        res.end();
+                    })
+                    // 등록된 리뷰의 ID, index를 가져오는 쿼리문
+                    
+                })
 
-        } else {
-            // 회원가입폼 테스트 구문 주석 처리
-            // req.on("data", chunk=>{
-            //     console.log(JSON.parse(chunk))
-            // })
-            // res.writeHead(200,{"Content-Type":"application/json"});
-            // const sender = {result : true}
-            // res.end(JSON.stringify(sender));
+            } else {
+                // 회원가입폼 테스트 구문 주석 처리
+                // req.on("data", chunk=>{
+                //     console.log(JSON.parse(chunk))
+                // })
+                // res.writeHead(200,{"Content-Type":"application/json"});
+                // const sender = {result : true}
+                // res.end(JSON.stringify(sender));
+            }
         }
-    }
 
 
-    if(req.url==="/src/img/star.png"&&req.method==="GET"){
-        const starImg=fs.readFileSync("./img/star.png");
-        res.end(starImg);
-    }
+        if (req.url === "/src/img/star.png" && req.method === "GET") {
+            const starImg = fs.readFileSync("./img/star.png");
+            res.end(starImg);
+        }
 
-})
+    })
 
 
 server.listen(3000, () => {
