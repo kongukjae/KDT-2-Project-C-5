d8564d57548c6909e48dffac464b0bc1ebb6c402
AmondChocolateShake <dongju230130@gmail.com>
2023-05-23 15:21:02 +0900

    fetch 설정 완료

--- components/DJ-ReviewInput.js
+++ components/DJ-ReviewInput.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { globalState } from '../src/Global';
 
 const container={
   display:"flex",
@@ -24,17 +25,30 @@ const inputContext={
 
 }
 
-
+//2가지 리뷰 작성 인풋을 담당하는 컴포넌트
 const ReviewInput=()=>{
+  const[r1,setR1]=useState('');
+  const[r2,setR2]=useState('');
+  
+  const reviewInput1=(e)=>{
+    setR1(e.target.value);
+    globalState.reviewData.review1=e.target.value;
+    console.log(globalState.reviewData.review1);
+  }
 
+  const reviewInput2=(e)=>{
+    setR2(e.target.value);
+    globalState.reviewData.review2=e.target.value;
+    
+  }
 
   return(
     <div style={container}>
       <div>
-        <input style={inputThree}></input>
+        <input style={inputThree} type='text' name='review1' placeholder='3줄 리뷰' onChange={reviewInput1}></input>
       </div>
       <div>
-        <input style={inputContext}></input>
+        <input style={inputContext} type='text' name='review2' placeholder='리뷰' onChange={reviewInput2}></input>
       </div>
     </div>
   );
