7ef50494e8872b9397ac699e7da6f7fa2c9cbe80
wjkk21 <verstand821@gmail.com>
2023-05-23 21:36:47 +0900

    알림창 멘트 추가

--- components/NotificationList.js
+++ components/NotificationList.js
@@ -5,6 +5,8 @@ const NotificationList = () => {
   return (
     <div style={{ backgroundColor: "#CAC2C2" }}>
       <div>'jiny273588'님이 당신을 팔로우 하셨습니다.</div>
+      <div>'verstand821'님이 당신을 팔로우 하셨습니다.</div>
+      <div>'제인'님이 좋아요를 누르셨습니다.</div>
     </div>
   );
 };
