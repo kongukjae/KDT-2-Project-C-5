76c76148b66268b64fb6d68ea6b214e76ec28687
wjkk21 <verstand821@gmail.com>
2023-05-23 21:41:50 +0900

    NotificationList style 변경

--- components/NotificationList.js
+++ components/NotificationList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import myImage from "../src/img/goose.jpg";
 
+const NotificationListstyle ={
+  backgroundColor: "#CAC2C2" 
+}
+
 const NotificationList = () => {
   return (
-    <div style={{ backgroundColor: "#CAC2C2" }}>
+    <div style={NotificationListstyle}>
       <div>'jiny273588'님이 당신을 팔로우 하셨습니다.</div>
       <div>'verstand821'님이 당신을 팔로우 하셨습니다.</div>
       <div>'제인'님이 좋아요를 누르셨습니다.</div>
