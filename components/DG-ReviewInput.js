abd9e83f3e9c1d91518b815d683dadd2267d73dd
DGChoi3904 <dgchoi3904@gmail.com>
2023-05-24 17:54:16 +0900

    테스트를 위해 파일명 수정 및 코드 추가.

---
+++ components/DG-ReviewInput.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const container={
+  display:"flex",
+  flexDirection:"column",
+  justifyContent:"space-around",
+  width:"100%",
+  height:"60%"
+}
+
+const inputThree={
+  width:"100%",
+  height:"70px",
+  border:"none",
+  borderRadius:"5px",
+
+}
+
+const inputContext={
+  width:"100%",
+  height:"150px",
+  border:"none",
+  borderRadius:"5px"
+
+}
+
+
+const ReviewInput=()=>{
+
+
+  return(
+    <div style={container}>
+      <label htmlFor="summary">
+        Summary:
+        <input type="text" id="summary" name="summery" style={inputThree} />
+      </label>
+      <label htmlFor="body">
+        Body:
+        <input type="text" id="body" name="body" style={inputContext} />
+      </label>
+    </div>
+  );
+}
+
