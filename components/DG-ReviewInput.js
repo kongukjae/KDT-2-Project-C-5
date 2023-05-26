47a8d2a6c41795ecd0c742f499dbd6c511e6816c
leejaekwon <nowkwon97@gmail.com>
2023-05-26 15:53:39 +0900

    main merge용 브랜치 생성 작업 완료

--- components/DG-ReviewInput.js
+++ components/DG-ReviewInput.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { globalState } from '../src/Global';
 
 const container={
   display:"flex",
@@ -24,21 +25,36 @@ const inputContext={
 
 }
 
-
+//2가지 리뷰 작성 인풋을 담당하는 컴포넌트
 const ReviewInput=()=>{
+  const[r1,setR1]=useState('');
+  const[r2,setR2]=useState('');
+  
+  const reviewInput1=(e)=>{
+    setR1(e.target.value);
+    globalState.reviewData.review1=e.target.value;
+    console.log(globalState.reviewData.review1);
+  }
 
+  const reviewInput2=(e)=>{
+    setR2(e.target.value);
+    globalState.reviewData.review2=e.target.value;
+    
+  }
 
   return(
     <div style={container}>
       <label htmlFor="summary">
         Summary:
-        <input type="text" id="summary" name="summery" style={inputThree} />
+        <input type="text" id="summary" name="summery" style={inputThree} placeholder='3줄 리뷰' onChange={reviewInput1} />
       </label>
       <label htmlFor="body">
         Body:
-        <input type="text" id="body" name="body" style={inputContext} />
+        <input type="text" id="body" name="body" style={inputContext} placeholder='리뷰' onChange={reviewInput2} />
       </label>
     </div>
   );
 }
 
+
+export default ReviewInput;
