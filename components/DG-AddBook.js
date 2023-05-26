5050ccc04a3f3d921643ad215f8a1e9ed6db2072
DGChoi3904 <dgchoi3904@gmail.com>
2023-05-26 13:07:44 +0900

    각자의 작업 merge test Fixes #101

--- components/DG-AddBook.js
+++ components/DG-AddBook.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 
 import SearchBar from './DJ-SearchBar'
+import SearchBooks from '../JE_SelectBooks'
 import './AddBook.css'
 
 
@@ -44,7 +45,7 @@ const AddBook=()=>{
     <div style={crossBox}>
       <div id="cross"></div>
     </div>
-    <label htmlFor="booktitle">
+    {/* <label htmlFor="booktitle">
       Book Title:
       <input type="text" id="booktitle" name="booktitle"/>
     </label>
@@ -67,11 +68,12 @@ const AddBook=()=>{
     <label htmlFor="isbn">
       ISBN:
       <input type="text" id="isbn" name="isbn" />
-    </label>
+    </label> */}
 
     <div style={searchBox}>
-      {/* 자동완성 기능이 담겨있는 도서 검색 바를 여기에 넣어야함 */}
-      <SearchBar />
+      <SearchBooks />
+      {/* <SearchBar /> */}
+      
     </div>
   </div>
   );
