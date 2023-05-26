422c9e8244cee50468f5497190c679fb7869f3e2
wjkk21 <verstand821@gmail.com>
2023-05-26 15:27:42 +0900

    Global.js tags 추가 덮어쓰기

--- src/Global.js
+++ src/Global.js
@@ -9,12 +9,6 @@ import bookcover6 from '../src/img/book-cover2.jpg';
 
 export let globalState = {
   tags: "",
-  reviewData:{
-    bookTitle:"",
-    review1:"",
-    review2:"",
-    tag:""
-  },
   DB: {
     "bookreview": [
       {
@@ -170,8 +164,7 @@ export let globalState = {
       }
     ]
   }
-};
-  
+  }
 export function getUserInfo() {
   return globalState.DB.userinfo;
 }
